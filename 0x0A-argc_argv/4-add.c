0d919b70e3ee5b7a0f0839824ab9c5b13e7b4474
praise <praisewizzy@gmail.com>
2023-03-10 09:18:39 -0800

    T2

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,84 +1,46 @@
 #include <stdio.h>
-	#include <stdlib.h>
-	#include <ctype.h>
-	#include <string.h>
-	/**
-	 * check_num - check - string there are digit
-	 * @str: array str
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int check_num(char *str)
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - entry point
+ * Description: adds 2 positive numbers
+ * @argc: arguement count
+ * @argv: argument array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum;
+	int num;
+	int arg_index;
+	int char_index;
+
+	sum = 0;
+	if (argc == 1)
 	{
-		/*Declaring variables*/
-		unsigned int count;
-
-
-		count = 0;
-		while (count < strlen(str)) /*count string*/
-
-
-		{
-			if (!isdigit(str[count])) /*check if str there are digit*/
-			{
-				return (0);
-			}
-
-
-			count++;
-		}
-		return (1);
+		printf("0\n");
+		return (0);
 	}
-
-
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
-
-
-	int main(int argc, char *argv[])
-
-
+	for (arg_index = 1; arg_index < argc; arg_index++)
 	{
-
-
-		/*Declaring variables*/
-		int count;
-		int str_to_int;
-		int sum = 0;
-
-
-		count = 1;
-		while (count < argc) /*Goes through the whole array*/
+		for (char_index = 0; argv[arg_index][char_index] != '\0'; char_index++)
 		{
-			if (check_num(argv[count]))
-
-
-			{
-				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-				sum += str_to_int;
-			}
-
-
-			/*Condition if one of the number contains symbols that are not digits*/
-			else
+			if (!isdigit(argv[arg_index][char_index]))
 			{
 				printf("Error\n");
 				return (1);
 			}
-
-
-			count++;
 		}
-
-
-		printf("%d\n", sum); /*print sum*/
-
-
-		return (0);
+		num = atoi(argv[arg_index]);
+		if (num < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += num;
 	}
-
+	printf("%d\n", sum);
+	return (0);
+}
